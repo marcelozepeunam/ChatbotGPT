31accfa0e91e77cd7dc689619b68301e0af31eac
Marcelo Zepeda <marcelozepe.unam@gmail.com>
2023-05-22 14:27:27 -0600

    Creando el chatbot real

--- chatbot.py
+++ chatbot.py
@@ -1,31 +1,39 @@
 import os 
 import openai
+import spacy
 from dotenv import load_dotenv
 
+#Importamos bibliotecas y configuramos la clave 
 load_dotenv()
-
-#Preparamos la petición
 api_key=os.getenv("OPENAI_API_KEY")
 openai.api_key=api_key
 
-modelo="text-davinci-002"
-prompt="De que se trata la película de el padrino?"
+#Función para peticiones
+def preguntar_chat_gpt(prompt, modelo="text-davinci-002"):
+    respuesta=openai.Completion.create(
+        engine=modelo,
+        prompt=prompt,
+        n=1,
+        max_tokens=150,
+        temperature=0.9
+    )
+    return respuesta.choices[0].text.strip()
 
-#Enviamos una petición
-respuesta=openai.Completion.create(
-    engine=modelo, #Davinci
-    prompt=prompt, #Indicación que se le dara a la IA 
-    n=1, #Número de respuestas que queremos recibir
-    temperature=1, #Creatividad de la respuesta se mide en un rango de [0-1]
-    max_tokens=100 #Largo de la respuesta 
-)
+#Funcionamiento básico
+print('Bienvenido a nuestro chatbot básico. Escribe "salir" cuando quieras terminar')
+while True:
+    ingreso_usuario=input("\nTú: ")
+    if ingreso_usuario.lower()=="salir":
+        print("Hasta luego =D")
+        break
+    
+    else:
+        prompt=(f"El usaurio pregunta: {ingreso_usuario}\nChatGPT responde: ")
+        respuesta_gpt=preguntar_chat_gpt(prompt)
+        print(f"Chatbot: {respuesta_gpt}")
 
-#Recibiendo la respuesta 
-'''Nota: 
-Como recibimos un archivo tipo JSON, debemos darle formato a nuestra respuesta, de la sig manera:
-Obtenemos la primera categoria de las respuestas dadas con choices e índice 0
-Con el método strip eliminaremos de cualquier string los espacios en blanco que hay antes y después '''
-texto_generado=respuesta.choices[0].text.strip()
-print(texto_generado)
+#Ejecución de chatbot
+preguntar_chat_gpt()
+
 
 
